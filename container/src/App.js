294d0c6598a303ee3d633aee1a6f7eb32205fbe3
rahulfrontend1-cmd <rahul.frontend1@gmail.com>
2026-09-06 19:14:21 +0530

    added dashboard

--- container/src/App.js
+++ container/src/App.js
@@ -1,22 +1,32 @@
-import React, { useState, lazy, Suspense } from "react";
+import React, { useState, lazy, Suspense, useEffect } from "react";
 import Header from "./components/Header";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
+import { createBrowserHistory } from "history";
 import Progress from "./components/Progress";
 
 const LazyAuthApp = lazy(() => import("./components/AuthApp"));
 const LazyMarketingApp = lazy(() => import("./components/MarketingApp"));
+const LazyDashboardApp = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 
+const history = createBrowserHistory();
+
 const App = () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push("/dashboard");
+    }
+  }, [isSignedIn]);
+
   return (
     <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
           <Suspense fallback={<Progress />}>
@@ -24,12 +34,16 @@ const App = () => {
               <Route path="/auth">
                 <LazyAuthApp onSignIn={() => setIsSignedIn(true)} />
               </Route>
+              <Route path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
+                <LazyDashboardApp />
+              </Route>
               <Route path="/" component={LazyMarketingApp} />
             </Switch>
           </Suspense>
           
         </div>
-      </BrowserRouter>
+      </Router>
     </StylesProvider>
   );
 }
