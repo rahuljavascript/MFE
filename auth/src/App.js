76446d98ab7ab8df4acf025ec7801cab6fc1bbfe
rahulfrontend1-cmd <rahul.frontend1@gmail.com>
2026-09-06 17:00:44 +0530

    import fix

--- auth/src/App.js
+++ auth/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Switch, Route, Router } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
-import SignIn from "./components/SignIn";
-import SignUp from "./components/SignUp";
+import SignIn from "./components/Signin";
+import SignUp from "./components/Signup";
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "au",
